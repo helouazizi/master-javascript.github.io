6182cb6567ea4125ff849f3d159707c71f8e1de2
ouazizi <ouazizi2028@gmail.com>
2024-03-25 16:46:11 +0000

    finish js code for counter project

--- script.js
+++ script.js
@@ -13,4 +13,35 @@ shownBtn.addEventListener('click',function(){
         links.classList.add('hide')
     }*/
     
-}) 
+}) ;
+
+
+// ======================= this part for counter project ===============//
+
+
+let counter = document.getElementById('count');
+let decrease = document.getElementById('decrease');
+let reset = document.getElementById('reset');
+let increase = document.getElementById('increase');
+
+console.log(counter,decrease,increase,reset);
+
+let i = 0;
+// lest create increament function 
+increase.addEventListener('click',function(){
+    counter.innerHTML =  i += 1;
+    
+});
+
+// lest creste descrement function  
+
+decrease.addEventListener('click',function(){
+    counter.innerHTML = i -= 1 ;
+});
+
+// lets creste reset function 
+
+reset.addEventListener('click',function(){
+    counter.innerHTML = 0;
+})
+
